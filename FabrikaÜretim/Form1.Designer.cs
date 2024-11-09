22cc35cebb3f013096075b76bd454cee392caa0d
Mahmut Abdulaziz BERK <m.abdulazizberk@icloud.com>
2024-11-09 19:40:41 +0300

    CSV Okuma Kısmen Yapıldı
    Anlamaya çalışıyorum.

--- FabrikaÜretim/Form1.Designer.cs
+++ FabrikaÜretim/Form1.Designer.cs
@@ -28,69 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.comboBoxLokasyon = new System.Windows.Forms.ComboBox();
+            this.textBoxSeriNo = new System.Windows.Forms.TextBox();
+            this.dataGridViewTablo = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SeriNumarasi = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.SeriNumarasi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonKaydet = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTablo)).BeginInit();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // comboBoxLokasyon
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.comboBoxLokasyon.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBoxLokasyon.FormattingEnabled = true;
+            this.comboBoxLokasyon.Items.AddRange(new object[] {
             "Kahramanmaraş Fabrikası",
             "Ankara Fabrikası",
             "İstanbul Fabrikası",
             "Osmaniye Fabrikası"});
-            this.comboBox1.Location = new System.Drawing.Point(392, 172);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(195, 37);
-            this.comboBox1.TabIndex = 0;
+            this.comboBoxLokasyon.Location = new System.Drawing.Point(412, 129);
+            this.comboBoxLokasyon.Name = "comboBoxLokasyon";
+            this.comboBoxLokasyon.Size = new System.Drawing.Size(349, 37);
+            this.comboBoxLokasyon.TabIndex = 0;
+            this.comboBoxLokasyon.SelectedIndexChanged += new System.EventHandler(this.comboBoxLokasyon_SelectedIndexChanged);
             // 
-            // textBox1
+            // textBoxSeriNo
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox1.Location = new System.Drawing.Point(392, 133);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 34);
-            this.textBox1.TabIndex = 1;
+            this.textBoxSeriNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxSeriNo.Location = new System.Drawing.Point(412, 173);
+            this.textBoxSeriNo.Name = "textBoxSeriNo";
+            this.textBoxSeriNo.Size = new System.Drawing.Size(349, 34);
+            this.textBoxSeriNo.TabIndex = 1;
+            this.textBoxSeriNo.TabStop = false;
+            this.textBoxSeriNo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSeriNo_KeyDown);
             // 
-            // dataGridView1
+            // dataGridViewTablo
             // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Name,
+            this.dataGridViewTablo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewTablo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTablo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
             this.SeriNumarasi});
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.14F);
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 288);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 150);
-            this.dataGridView1.TabIndex = 2;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.14F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewTablo.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewTablo.Location = new System.Drawing.Point(12, 288);
+            this.dataGridViewTablo.Name = "dataGridViewTablo";
+            this.dataGridViewTablo.RowHeadersVisible = false;
+            this.dataGridViewTablo.RowHeadersWidth = 51;
+            this.dataGridViewTablo.RowTemplate.Height = 24;
+            this.dataGridViewTablo.Size = new System.Drawing.Size(816, 150);
+            this.dataGridViewTablo.TabIndex = 2;
+            this.dataGridViewTablo.TabStop = false;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            // 
+            // SeriNumarasi
+            // 
+            this.SeriNumarasi.HeaderText = "SeriNumarasi";
+            this.SeriNumarasi.MinimumWidth = 6;
+            this.SeriNumarasi.Name = "SeriNumarasi";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(157, 138);
+            this.label1.Location = new System.Drawing.Point(156, 176);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(229, 29);
             this.label1.TabIndex = 3;
@@ -100,59 +117,64 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(165, 175);
+            this.label2.Location = new System.Drawing.Point(164, 130);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(221, 29);
             this.label2.TabIndex = 4;
             this.label2.Text = "Fabrika Lokasyonu:";
             // 
-            // button1
+            // buttonKaydet
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(343, 242);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 40);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Kaydet";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Name
-            // 
-            this.Name.HeaderText = "ID";
-            this.Name.MinimumWidth = 6;
-            this.Name.Name = "Name";
-            // 
-            // SeriNumarasi
-            // 
-            this.SeriNumarasi.HeaderText = "SeriNumarasi";
-            this.SeriNumarasi.MinimumWidth = 6;
-            this.SeriNumarasi.Name = "SeriNumarasi";
+            this.buttonKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonKaydet.Location = new System.Drawing.Point(304, 242);
+            this.buttonKaydet.Name = "buttonKaydet";
+            this.buttonKaydet.Size = new System.Drawing.Size(115, 40);
+            this.buttonKaydet.TabIndex = 5;
+            this.buttonKaydet.TabStop = false;
+            this.buttonKaydet.Text = "Kaydet";
+            this.buttonKaydet.UseVisualStyleBackColor = true;
+            this.buttonKaydet.Click += new System.EventHandler(this.buttonKaydet_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(33, 21);
+            this.label3.Location = new System.Drawing.Point(51, 23);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(755, 69);
             this.label3.TabIndex = 6;
             this.label3.Text = "DokuzTay Matbaa Tesisleri";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(425, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 40);
+            this.button1.TabIndex = 7;
+            this.button1.TabStop = false;
+            this.button1.Text = "Yükle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
+            this.AcceptButton = this.buttonKaydet;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(840, 450);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonKaydet);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridViewTablo);
+            this.Controls.Add(this.textBoxSeriNo);
+            this.Controls.Add(this.comboBoxLokasyon);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Fabrika Üretim Takip";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTablo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,15 +182,16 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBoxLokasyon;
+        private System.Windows.Forms.TextBox textBoxSeriNo;
+        private System.Windows.Forms.DataGridView dataGridViewTablo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn SeriNumarasi;
+        private System.Windows.Forms.Button buttonKaydet;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SeriNumarasi;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- FabrikaÜretim/Form1.cs
+++ FabrikaÜretim/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,132 @@ namespace FabrikaÜretim
         public Form1()
         {
             InitializeComponent();
+            dataGridViewTablo.Columns["ID"].ValueType = typeof(int);
+            dataGridViewTablo.Columns["SeriNumarasi"].ValueType = typeof(string);
+            label1.Visible = false;
+            textBoxSeriNo.Visible = false;
+            dataGridViewTablo.Visible = false;
+            buttonKaydet.Visible = false;   
+
+        }
+        private int idSayac = 1;
+
+        private void comboBoxLokasyon_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxLokasyon.SelectedItem != null)
+            {
+                comboBoxLokasyon.Enabled = false;
+                label1.Visible = true;
+                textBoxSeriNo.Visible = true;
+                dataGridViewTablo.Visible = true;
+                buttonKaydet.Visible = true;
+            }
+        }
+        private void textBoxSeriNo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Tab)
+            {
+                string seriNo = textBoxSeriNo.Text;
+
+                if (!string.IsNullOrEmpty(seriNo))
+                {
+                    dataGridViewTablo.Rows.Add(idSayac, seriNo);
+                    idSayac++;
+                    textBoxSeriNo.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen bir seri numarası girin.");
+                }
+            }
+        }
+
+        private void buttonKaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string filePath = Path.Combine(desktopPath, $"{DateTime.Now:yyyy}_{comboBoxLokasyon.Text}.csv");
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.WriteLine("ID;SeriNumarasi");
+
+                    foreach (DataGridViewRow row in dataGridViewTablo.Rows)
+                    {
+                        if (!row.IsNewRow)
+                        {
+                            string id = row.Cells["ID"].Value?.ToString();
+                            string seriNumarasi = row.Cells["SeriNumarasi"].Value?.ToString();
+
+                            writer.WriteLine($"{id};{seriNumarasi}");
+                        }
+                    }
+                }
+                MessageBox.Show("Veriler başarıyla kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Lokasyonu ComboBox'tan al
+                string lokasyon = comboBoxLokasyon.Text;
+                if (string.IsNullOrWhiteSpace(lokasyon))
+                {
+                    MessageBox.Show("Lütfen bir lokasyon seçin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Masaüstü yolunu al ve dosya yolunu oluştur
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string fileName = $"{DateTime.Now.Year}_{lokasyon}.csv";
+                string filePath = Path.Combine(desktopPath, fileName);
+
+                // Dosya mevcut mu kontrol et
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show($"Bu yıl ve lokasyona ait dosya bulunamadı: {fileName}", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                // CSV dosyasını oku ve DataGridView'e yükle
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    dataGridViewTablo.Rows.Clear(); // Mevcut verileri temizle
+                    dataGridViewTablo.Columns.Clear(); // Eski sütunları temizle
+
+                    // İlk satırda sütun başlıkları var, bunları ekle
+                    string headerLine = reader.ReadLine();
+                    if (headerLine != null)
+                    {
+                        string[] headers = headerLine.Split(',');
+                        foreach (string header in headers)
+                        {
+                            dataGridViewTablo.Columns.Add(header, header);
+                        }
+                    }
+
+                    // Diğer satırları oku ve DataGridView'e ekle
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] values = line.Split(',');
+                        dataGridViewTablo.Rows.Add(values);
+                    }
+                }
+
+                MessageBox.Show("Veriler başarıyla yüklendi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
